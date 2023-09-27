6f4c493f0def01569821ed3d674558b6e6117018
Jarek <jarek.janota8@gmail.com>
2023-09-27 21:10:09 +0200

    połączenie i przechowywanie postów w bazie danych

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
 
-posts = [
-    {
-        'author': 'stefan',
-        'title': 'tak',
-        'content': 'abc',
-        'date_posted': 'august 27, 2018'
-    },
-
-    {
-        'author': 'ziom',
-        'title': 'nie',
-        'content': 'cba',
-        'date_posted': 'august 12, 2018'
-    }
-]
+#posts = [
+#    {
+#        'author': 'stefan',
+#        'title': 'tak',
+#        'content': 'abc',
+#        'date_posted': 'august 27, 2018'
+#    },
+#
+#    {
+#        'author': 'ziom',
+#        'title': 'nie',
+#        'content': 'cba',
+#        'date_posted': 'august 12, 2018'
+#    }
+#]
 
 def home(request):
     context = {
-        'posts': posts
+        #'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
